dbb8ce693960709799669f45a00e63e05e5b11f0
Joao Eduardo Lima <joaoeduardo980@gmail.com>
2022-06-27 01:03:32 -0300

    feat: adicionar vagas na home

--- src/main/java/br/com/siva/controllers/HomeController.java
+++ src/main/java/br/com/siva/controllers/HomeController.java
@@ -1,16 +1,24 @@
 package br.com.siva.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.siva.repositories.VagaRepositorio;
+
 @Controller
 public class HomeController {
+	
+
+	@Autowired
+	private VagaRepositorio vagaRepo;
 	
 	@GetMapping("/")
 	public ModelAndView home() {		
 		ModelAndView mv = new ModelAndView("index.html");
 		mv.addObject("mensagem", "Futuro DASHBOARD");
+		mv.addObject("vagas", vagaRepo.findAll());
 		
 		return mv;
 	}	
